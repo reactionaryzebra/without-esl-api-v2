88c22aca2680f2910c89abbf43bf1f3a4e9181dd
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-03 07:02:54 -0700

    try explicitly naming methods

--- result/search.php
+++ result/search.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods: POST, OPTION");
 header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/Database.php';
