a3f8b61204cf126fb58b8ff29648fedc1e69dd4b
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-02 23:34:22 -0700

    allow all methods so as to prevent options from being blocked by CORS

--- result/search.php
+++ result/search.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/Database.php';
