a9b4650098a0769112939cc88ec922f1fe53684e
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-02 23:39:42 -0700

    try explicitly naming client origin in header

--- result/search.php
+++ result/search.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://without-esl.herokuapp.com");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json; charset=UTF-8");
