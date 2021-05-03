b9fb03a8aad3a5c22629c6a7f5ad6acec77d37fd
reactionaryzebra <reactionaryzebra@gmail.com>
2021-05-03 07:16:58 -0700

    Attempt conditional headers as seen in https://stackoverflow.com/questions/8719276/cross-origin-request-headerscors-with-php-headers

--- result/search.php
+++ result/search.php
@@ -1,7 +1,16 @@
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
+    
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/Database.php';
